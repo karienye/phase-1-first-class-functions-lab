d4771d084ba709d26f0fa0231874cf6c2c1bed7c
leah karienye <karienyeleah@gmail.com>
2022-06-09 01:02:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers =() =>[driver1, driver2, ...driver] ;{
+  console.log( returnFirstTwoDrivers.slice (0,2));
+}
+const returnLastTwoDrivers =() =>[driver1, driver2, ...driver] ;{
+    console.log( returnFirstTwoDrivers.slice (-1,-3));
+  }
+  function selectingDrivers() {[returnFirstTwoDrivers (), returnLastTwoDrivers()]} ;{
+    console.log();
+  }
+  function createFareMultiplier (a=8) {
+      let fareCalulator = a * 4;
+      createFareMultiplier(farecalulator)
+      console.log (fareCalculator)
+  };
+  function fareDoubler (fare = 10) {
+     let fareMultiplier = (fare * 2)
+console.log (fareMultiplier)
+}
+function fareTripler(fare = 12) {
+    const fareMultiplier = (fare * 3)
+console.log (fareMultiplier)
+}
+const selectDifferentDrivers =() =>[returnFirstTwoDrivers, returnLastTwoDrivers] ;{
+    console.log( returnFirstTwoDrivers || returnLastTwoDrivers);
+  }
